b278b2ee853878f0eaee9ecb8c0fb9ef4d1ea29b
Artem Vetik <artem.vetik58@gmail.com>
2024-02-03 15:59:36 +0300

    Create sample yandex cloud functions

---
+++ market-sample-api/Response.cs
@@ -0,0 +1,16 @@
+﻿namespace Agava.MarketSampleApi
+{
+    public class Response
+    {
+        public Response(uint statusCode, string reasonPhrase, string body)
+        {
+            StatusCode = statusCode;
+            Body = body;
+            ReasonPhrase = reasonPhrase;
+        }
+
+        public uint StatusCode { get; private set; }
+        public string ReasonPhrase { get; private set; }
+        public string Body { get; private set; }
+    }
+}
