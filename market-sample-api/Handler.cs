38c5da376bbca6cdbe148d09c96c50a7f89bde41
Artem Vetik <av.unitygames@gmail.com>
2024-02-03 15:59:36 +0300

    Create sample yandex cloud functions

---
+++ market-sample-api/Handler.cs
@@ -0,0 +1,41 @@
+﻿using Yandex.Cloud.Credentials;
+using Yandex.Cloud.Functions;
+using Ydb.Sdk.Auth;
+using Ydb.Sdk.Services.Table;
+using Ydb.Sdk;
+
+namespace Agava.MarketSampleApi
+{
+    internal partial class Handler : YcFunction<Request, Task<Response>>
+    {
+        public async Task<Response> FunctionHandler(Request request, Context context)
+        {
+            string ydbEndpoint = Environment.GetEnvironmentVariable("YdbEndpoint");
+            string ydbDatabase = Environment.GetEnvironmentVariable("YdbDatabase");
+
+            var token = new TokenProvider(new MetadataCredentialsProvider().GetToken());
+            var config = new DriverConfig(ydbEndpoint, ydbDatabase, token);
+
+            var driver = new Driver(config);
+            await driver.Initialize();
+
+            var tableClient = new TableClient(driver, new TableClientConfig());
+
+            try
+            {
+                BaseRequest requestHandler = request.method switch
+                {
+                    "LOGIN" => new LoginRequest(tableClient, request),
+                    _ => throw new InvalidOperationException($"Method {request.method} not found")
+                };
+
+                return await requestHandler.Handle();
+            }
+            finally
+            {
+                tableClient.Dispose();
+                driver.Dispose();
+            }
+        }
+    }
+}
---
+++ market-sample-api/LoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using Ydb.Sdk.Services.Table;
+
+namespace Agava.MarketSampleApi
+{
+    internal class LoginRequest : BaseRequest
+    {
+        public LoginRequest(TableClient tableClient, Request request)
+            : base(tableClient, request)
+        { }
+
+        protected override async Task<Response> Handle(TableClient client, Request request)
+        {
+            await Task.Yield();
+            return new Response(0, "Success", $"method: {request.method}, body: {request.body}, access token: {request.access_token}");
+        }
+    }
+}
